55acad93281f05352b6a02ef914ab58d9909b3d4
Seyedyt <87230961+Seyedyt@users.noreply.github.com>
2024-06-20 03:45:23 -0700

    Add files via upload
    resolving errors in the cli.sh

--- Q2/cli.sh
+++ Q2/cli.sh
@@ -12,34 +12,45 @@ create_container() {
     fi
 
     # Create the new namespaces and run a shell
-    unshare --fork --pid --mount-proc --net --uts --mount /bin/bash &
-    sleep 1
-    pid=$!
-
-    # Enter the new namespaces
-    nsenter --target $pid --uts --mount --net --pid /bin/bash <<EOF
-    # Set the hostname
+    unshare --fork --pid --mount-proc --net --uts --mount /bin/bash -c "
+    set -e
+    echo 'Setting hostname'
     hostname $hostname
 
-    # Mount the new filesystem
+    echo 'Mounting new filesystem'
     mkdir -p /mnt/oldroot
     mount --bind $UBUNTU_ROOTFS_PATH /mnt
     pivot_root /mnt /mnt/oldroot
 
-    # Unmount the old root
-    umount /mnt/oldroot
-    rmdir /mnt/oldroot
+    echo 'Changing directory to new root'
+    cd /
 
-    # Apply memory limit if specified
-    if [ ! -z "$memory_limit" ]; then
+    echo 'Unmounting old root'
+    umount -l /oldroot
+
+    echo 'Removing old root directory'
+    rmdir /oldroot || true
+
+    if [ ! -z '$memory_limit' ]; then
+        echo 'Setting memory limit'
         mkdir -p /sys/fs/cgroup/memory/container
         echo $((memory_limit * 1024 * 1024)) > /sys/fs/cgroup/memory/container/memory.limit_in_bytes
         echo $$ > /sys/fs/cgroup/memory/container/cgroup.procs
     fi
 
-    # Start a new bash shell in the container
-    exec /bin/bash
-EOF
+    echo 'Entering new namespace'
+    exec /bin/bash" &
+    pid=$!
+
+    # Wait for the process to start and enter the namespaces
+    sleep 1
+
+    if [ -d "/proc/$pid/ns" ]; then
+        nsenter --target $pid --uts --mount --net --pid /bin/bash
+    else
+        echo "Failed to create container: process $pid does not exist"
+        exit 1
+    fi
 }
 
 if [ $# -lt 1 ]; then
