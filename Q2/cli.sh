8b34d5b8e33ef888c7995f5c46e5a08066c06377
Seyedyt <87230961+Seyedyt@users.noreply.github.com>
2024-06-20 03:27:53 -0700

    Add files via upload
    adding the memory limitation

--- Q2/cli.sh
+++ Q2/cli.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-UBUNTU_ROOTFS_PATH="/ubuntu_rootfs"  # Change this to the actual path
+UBUNTU_ROOTFS_PATH="/ubuntu_rootfs"  
 
 create_container() {
     hostname=$1
+    memory_limit=$2
     echo "Creating container with hostname: $hostname"
+    if [ ! -z "$memory_limit" ]; then
+        echo "Setting memory limit: ${memory_limit}MB"
+    fi
     unshare --fork --pid --mount-proc --net --uts --mount /bin/bash &
     sleep 1
     pid=$!
@@ -15,15 +19,21 @@ create_container() {
     pivot_root /mnt /mnt/oldroot
     umount /mnt/oldroot
     rmdir /mnt/oldroot
+    if [ ! -z "$memory_limit" ]; then
+        mkdir -p /sys/fs/cgroup/memory/container
+        echo $((memory_limit * 1024 * 1024)) > /sys/fs/cgroup/memory/container/memory.limit_in_bytes
+        echo $pid > /sys/fs/cgroup/memory/container/cgroup.procs
+    fi
     echo "Entering new namespace"
     nsenter --target $pid --all /bin/bash
 }
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <hostname>"
+    echo "Usage: $0 <hostname> [memory_limit]"
     exit 1
 fi
 
 hostname=$1
-create_container $hostname
+memory_limit=$2
+create_container $hostname $memory_limit
 
