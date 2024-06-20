2307ca6fc69ce28abd8a72fe543ec767f08abf7b
Seyedyt <87230961+Seyedyt@users.noreply.github.com>
2024-06-20 06:16:50 -0700

    Add files via upload
    uploading Question 3 files

---
+++ Q3/app.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+
+hostName = "localhost"
+serverPort = 8000
+
+class MyServer(BaseHTTPRequestHandler):
+
+    # Initialize class variables
+    ok = True
+    status_message = "OK"
+
+    def do_GET(self):
+        self.send_response(200 if MyServer.ok else 201)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps({"status": MyServer.status_message}).encode("utf-8"))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data.decode('utf-8'))
+
+        if "status" in data:
+            MyServer.status_message = data["status"]
+            MyServer.ok = (MyServer.status_message == "OK")
+
+        self.send_response(201)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps({"status": MyServer.status_message}).encode("utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
